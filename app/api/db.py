6914fa5afb0848390289a14f0490eb9c605d3260
fyambos <feliciteyambos@gmail.com>
2023-12-14 09:56:38 +0100

    movie service, dockerfile and os url

--- app/api/db.py
+++ app/api/db.py
@@ -1,11 +1,13 @@
-#~/movie-service/app/api/db.py
+#~/python-microservices/movie-service/app/api/db.py
 
-from sqlalchemy import (Column, Integer, MetaData, String, Table,
+import os
+
+from sqlalchemy import (Column, DateTime, Integer, MetaData, String, Table,
                         create_engine, ARRAY)
 
 from databases import Database
 
-DATABASE_URL = 'postgresql://movie_user:movie_password@localhost/movie_db'
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
@@ -17,7 +19,7 @@ movies = Table(
     Column('name', String(50)),
     Column('plot', String(250)),
     Column('genres', ARRAY(String)),
-    Column('casts', ARRAY(String))
+    Column('casts_id', ARRAY(Integer))
 )
 
 database = Database(DATABASE_URL)
--- app/api/models.py
+++ app/api/models.py
@@ -1,4 +1,4 @@
-#~/movie-service/app/api/models.py
+#~/python-microservices/movie-service/app/api/models.py
 
 from pydantic import BaseModel
 from typing import List, Optional
@@ -7,7 +7,7 @@ class MovieIn(BaseModel):
     name: str
     plot: str
     genres: List[str]
-    casts: List[str]
+    casts_id: List[int]
 
 
 class MovieOut(MovieIn):
@@ -18,4 +18,4 @@ class MovieUpdate(MovieIn):
     name: Optional[str] = None
     plot: Optional[str] = None
     genres: Optional[List[str]] = None
-    casts: Optional[List[str]] = None
+    casts_id: Optional[List[int]] = None
